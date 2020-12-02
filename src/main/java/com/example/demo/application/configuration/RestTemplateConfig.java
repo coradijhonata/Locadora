7a2a2d3c6aa14eec01a328ca68314ba2aaa946a0
Jhonata <coradi.jhonata@gmail.com>
2020-12-02 20:57:37 -0300

    LOC-02 - Configurando os endPoints dos filmes no swagger

---
+++ src/main/java/com/example/demo/application/configuration/RestTemplateConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.application.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    /*
+    * usar o dependecyFactory para configurar o restTemplateBuilder
+    *
+    * */
+
+}
---
+++ src/main/java/com/example/demo/application/factories/DependencyFactory.java
@@ -0,0 +1,11 @@
+package com.example.demo.application.factories;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DependencyFactory {
+
+    //configuraçõa do resTemplateConnectTimeOut
+    //configuração do restTemplateReadTimeOut    colocar um @Value - Pesquisar sobre o que é value em relação aos rests
+
+}
