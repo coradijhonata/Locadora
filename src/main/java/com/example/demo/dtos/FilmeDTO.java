033d5679639d7a1ec8c07fa23970d69159e4d178
Jhonata <coradi.jhonata@gmail.com>
2020-12-22 17:51:06 -0300

    LOC-06 - [HOTFIX] Criação construtores nos DTOs

--- src/main/java/com/example/demo/dtos/FilmeDTO.java
+++ src/main/java/com/example/demo/dtos/FilmeDTO.java
@@ -1,5 +1,6 @@
 package com.example.demo.dtos;
 
+import com.example.demo.domain.entities.Filme;
 import com.example.demo.domain.entities.GeneroFilme;
 import com.example.demo.domain.entities.TipoFilme;
 
@@ -8,10 +9,18 @@ public class FilmeDTO {
     private String nome;
     private String diretor;
     private int ano_lancamento;
-    private TipoFilme tipofilme;
+    private TipoFilme tipoFilme;
     private int qtd_volumes_disponiveis;
     private GeneroFilme generoFilme;
 
+    public FilmeDTO(Filme filme){
+        this.nome = filme.getNome();
+        this.diretor = filme.getDiretor();
+        this.ano_lancamento = filme.getAno_lancamento();
+        this.tipoFilme = filme.getTipoFilme();
+        this.qtd_volumes_disponiveis = filme.getQtd_volumes_disponiveis();
+        this.generoFilme = filme.getGeneroFilme();
 
+    }
 
 }
--- src/main/java/com/example/demo/dtos/LocacaoDTO.java
+++ src/main/java/com/example/demo/dtos/LocacaoDTO.java
@@ -2,6 +2,7 @@ package com.example.demo.dtos;
 
 import com.example.demo.domain.entities.Cliente;
 import com.example.demo.domain.entities.Filme;
+import com.example.demo.domain.entities.Locacao;
 import com.example.demo.domain.entities.StatusLocacao;
 
 import java.util.Date;
@@ -13,5 +14,14 @@ public class LocacaoDTO {
     private Date dtLocacao;
     private Date dtDevolucao;
     private StatusLocacao statusLocacao;
+
+    public LocacaoDTO(Locacao locacao) {
+        this.filme = locacao.getFilme();
+        this.cliente = locacao.getCliente();
+        this.dtLocacao = locacao.getDtLocacao();
+        this.dtDevolucao = locacao.getDtDevolucao();
+        this.statusLocacao = locacao.getStatusLocacao();
+
+    }
 
 }
